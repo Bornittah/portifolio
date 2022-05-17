e6314754f55ac715d51cae9739815243b0a8e6fa
Bornittah <bornittah@gmail.com.com>
2022-05-17 16:13:51 +0300

    add popup menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,21 @@
-const hamburger = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
 
 hamburger.addEventListener('click', ()=>{
 hamburger.classList.toggle('active');
+overlay.classList.toggle('active');
+menu.classList.toggle('active');
+logo.classList.toggle('active');
 });
+
+document.querySelector('.menu')
+.forEach(n=>n.addEventListener('click', ()=>{
+hamburger.classList.remove('active');
+menu.classList.remove('active');
+logo.classList.remove('active');
+// logo.style.display="none";
+// logo.style.display="none";
+}));
+
