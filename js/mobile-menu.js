4db60fb13b2ea8b67186d139c08935a551aaf94e
Bornittah <bornittah@gmail.com.com>
2022-05-17 17:52:03 +0300

    update popup menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,21 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.menu');
+const menuLink = document.querySelector('.menu-link');
 const logo = document.querySelector('.logo');
 
-hamburger.addEventListener('click', ()=>{
-hamburger.classList.toggle('active');
-overlay.classList.toggle('active');
-menu.classList.toggle('active');
-logo.classList.toggle('active');
-});
+    hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    overlay.classList.toggle('active');
+    menu.classList.toggle('active');
+    logo.classList.toggle('active');
+    });
 
-document.querySelector('.menu')
-.forEach(n=>n.addEventListener('click', ()=>{
-hamburger.classList.remove('active');
-menu.classList.remove('active');
-logo.classList.remove('active');
-// logo.style.display="none";
-// logo.style.display="none";
-}));
+    menuLink.forEach((link)=>{
+    link.addEventListener('click', ()=>{
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
+    logo.classList.remove('active');
+    overlay.classList.remove('active');
+
+});
+});
 
